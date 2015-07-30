886ec207b3fcc9c9d2fdd26ba8a0f0af03e6847c
Joylice <493792523@qq.com>
2015-07-30 15:20:38 +0800

    DataTranscription
    DataTranscription

---
+++ DataTranscription/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace DataTranscription
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.combDataBaseOld = new System.Windows.Forms.ComboBox();
+            this.comOldtable = new System.Windows.Forms.ComboBox();
+            this.combDataBaseNew = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comNewtable = new System.Windows.Forms.ComboBox();
+            this.btn_biaoOld = new System.Windows.Forms.Button();
+            this.btn_biaoNew = new System.Windows.Forms.Button();
+            this.btn_OldZiDuan = new System.Windows.Forms.Button();
+            this.btn_NewZiDuan = new System.Windows.Forms.Button();
+            this.btn_StartZhuanLu = new System.Windows.Forms.Button();
+            this.OldZiDuan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NewZiDuan = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.isTrue = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "旧数据库";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "表";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "字段";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.OldZiDuan,
+            this.NewZiDuan,
+            this.isTrue});
+            this.dataGridView1.Location = new System.Drawing.Point(79, 135);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(673, 275);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dataGridView1_DataBindingComplete);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(382, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "新数据库";
+            // 
+            // combDataBaseOld
+            // 
+            this.combDataBaseOld.FormattingEnabled = true;
+            this.combDataBaseOld.Location = new System.Drawing.Point(111, 32);
+            this.combDataBaseOld.Name = "combDataBaseOld";
+            this.combDataBaseOld.Size = new System.Drawing.Size(121, 20);
+            this.combDataBaseOld.TabIndex = 7;
+            // 
+            // comOldtable
+            // 
+            this.comOldtable.FormattingEnabled = true;
+            this.comOldtable.Location = new System.Drawing.Point(111, 84);
+            this.comOldtable.Name = "comOldtable";
+            this.comOldtable.Size = new System.Drawing.Size(121, 20);
+            this.comOldtable.TabIndex = 8;
+            // 
+            // combDataBaseNew
+            // 
+            this.combDataBaseNew.FormattingEnabled = true;
+            this.combDataBaseNew.Location = new System.Drawing.Point(441, 32);
+            this.combDataBaseNew.Name = "combDataBaseNew";
+            this.combDataBaseNew.Size = new System.Drawing.Size(121, 20);
+            this.combDataBaseNew.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(383, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "表";
+            // 
+            // comNewtable
+            // 
+            this.comNewtable.FormattingEnabled = true;
+            this.comNewtable.Location = new System.Drawing.Point(441, 84);
+            this.comNewtable.Name = "comNewtable";
+            this.comNewtable.Size = new System.Drawing.Size(121, 20);
+            this.comNewtable.TabIndex = 11;
+            // 
+            // btn_biaoOld
+            // 
+            this.btn_biaoOld.Location = new System.Drawing.Point(238, 32);
+            this.btn_biaoOld.Name = "btn_biaoOld";
+            this.btn_biaoOld.Size = new System.Drawing.Size(120, 23);
+            this.btn_biaoOld.TabIndex = 12;
+            this.btn_biaoOld.Text = "读取此数据库的表";
+            this.btn_biaoOld.UseVisualStyleBackColor = true;
+            this.btn_biaoOld.Click += new System.EventHandler(this.btn_biaoOld_Click);
+            // 
+            // btn_biaoNew
+            // 
+            this.btn_biaoNew.Location = new System.Drawing.Point(594, 31);
+            this.btn_biaoNew.Name = "btn_biaoNew";
+            this.btn_biaoNew.Size = new System.Drawing.Size(136, 23);
+            this.btn_biaoNew.TabIndex = 13;
+            this.btn_biaoNew.Text = "读取此数据库内的表";
+            this.btn_biaoNew.UseVisualStyleBackColor = true;
+            this.btn_biaoNew.Click += new System.EventHandler(this.btn_biaoNew_Click);
+            // 
+            // btn_OldZiDuan
+            // 
+            this.btn_OldZiDuan.Location = new System.Drawing.Point(238, 81);
+            this.btn_OldZiDuan.Name = "btn_OldZiDuan";
+            this.btn_OldZiDuan.Size = new System.Drawing.Size(120, 23);
+            this.btn_OldZiDuan.TabIndex = 14;
+            this.btn_OldZiDuan.Text = "读取该表字段";
+            this.btn_OldZiDuan.UseVisualStyleBackColor = true;
+            this.btn_OldZiDuan.Click += new System.EventHandler(this.btn_OldZiDuan_Click);
+            // 
+            // btn_NewZiDuan
+            // 
+            this.btn_NewZiDuan.Location = new System.Drawing.Point(594, 80);
+            this.btn_NewZiDuan.Name = "btn_NewZiDuan";
+            this.btn_NewZiDuan.Size = new System.Drawing.Size(118, 23);
+            this.btn_NewZiDuan.TabIndex = 15;
+            this.btn_NewZiDuan.Text = "读取该表字段";
+            this.btn_NewZiDuan.UseVisualStyleBackColor = true;
+            this.btn_NewZiDuan.Click += new System.EventHandler(this.btn_NewZiDuan_Click);
+            // 
+            // btn_StartZhuanLu
+            // 
+            this.btn_StartZhuanLu.Location = new System.Drawing.Point(360, 438);
+            this.btn_StartZhuanLu.Name = "btn_StartZhuanLu";
+            this.btn_StartZhuanLu.Size = new System.Drawing.Size(75, 23);
+            this.btn_StartZhuanLu.TabIndex = 16;
+            this.btn_StartZhuanLu.Text = "开始转录";
+            this.btn_StartZhuanLu.UseVisualStyleBackColor = true;
+            this.btn_StartZhuanLu.Click += new System.EventHandler(this.btn_StartZhuanLu_Click);
+            // 
+            // OldZiDuan
+            // 
+            this.OldZiDuan.DataPropertyName = "name";
+            this.OldZiDuan.HeaderText = "字段";
+            this.OldZiDuan.Name = "OldZiDuan";
+            // 
+            // NewZiDuan
+            // 
+            this.NewZiDuan.HeaderText = "对应新数据库字段";
+            this.NewZiDuan.Name = "NewZiDuan";
+            this.NewZiDuan.Width = 200;
+            // 
+            // isTrue
+            // 
+            this.isTrue.HeaderText = "是否启用";
+            this.isTrue.Name = "isTrue";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 491);
+            this.Controls.Add(this.btn_StartZhuanLu);
+            this.Controls.Add(this.btn_NewZiDuan);
+            this.Controls.Add(this.btn_OldZiDuan);
+            this.Controls.Add(this.btn_biaoNew);
+            this.Controls.Add(this.btn_biaoOld);
+            this.Controls.Add(this.comNewtable);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.combDataBaseNew);
+            this.Controls.Add(this.comOldtable);
+            this.Controls.Add(this.combDataBaseOld);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox combDataBaseOld;
+        private System.Windows.Forms.ComboBox comOldtable;
+        private System.Windows.Forms.ComboBox combDataBaseNew;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comNewtable;
+        private System.Windows.Forms.Button btn_biaoOld;
+        private System.Windows.Forms.Button btn_biaoNew;
+        private System.Windows.Forms.Button btn_OldZiDuan;
+        private System.Windows.Forms.Button btn_NewZiDuan;
+        private System.Windows.Forms.Button btn_StartZhuanLu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OldZiDuan;
+        private System.Windows.Forms.DataGridViewComboBoxColumn NewZiDuan;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn isTrue;
+    }
+}
+
---
+++ DataTranscription/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace DataTranscription
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BindDataBase(combDataBaseNew, 0, "");
+            BindDataBase(combDataBaseOld, 0, "");
+            
+        }
+        //获取新数据库 0 是数据库 1是 表
+        private void BindDataBase(ComboBox combDataBase, int style, string database)
+        {
+            switch (style)
+            {
+                case 0:
+                    {
+                        string strSql = "select name from sysdatabases order by name";
+                        combDataBase.DataSource = this.GetDataBases(strSql).Tables[0];
+                        combDataBase.DisplayMember = "name";
+                        combDataBase.ValueMember = "name";
+                        break;
+                    }
+                case 1:
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.AppendFormat("use {0}", database);
+                        sb.AppendFormat(" SELECT Name from sysobjects Where Type='U' ORDER BY Name");
+                        combDataBase.DataSource = this.GetDataBases(sb.ToString()).Tables[0];
+                        combDataBase.ValueMember = "name";
+                        combDataBase.DisplayMember = "name";
+                        break;
+                    }
+            }
+        }
+        //获取数据库连接
+        private SqlConnection GetConnections()
+        {
+            SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder();
+            sqlsb.DataSource = "localhost";
+            sqlsb.IntegratedSecurity = true;
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = sqlsb.ConnectionString;
+            return conn;
+        }
+        //根据查询语句，获取对应的数据集
+        private DataSet GetDataBases(string strSql)
+        {
+            SqlDataAdapter sda = new SqlDataAdapter();
+            SqlCommand cmd = new SqlCommand();
+            DataSet ds = new DataSet();
+            using (SqlConnection conn = this.GetConnections())
+            {
+                conn.Open();
+                cmd.CommandText = strSql;
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = conn;
+                sda.SelectCommand = cmd;
+                sda.Fill(ds, "databases");
+                conn.Close()        ;
+               
+            }
+            return ds;
+        }
+        private int GetDoIt(string strSql)
+        {
+            SqlCommand cmd = new SqlCommand();
+            object b = null;
+            using (SqlConnection conn = this.GetConnections())
+            {
+                conn.Open();
+                cmd.CommandText = strSql;
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = conn;
+                b= cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+           return Convert.ToInt32(b);
+        }
+        private void btn_biaoOld_Click(object sender, EventArgs e)
+        {
+            BindDataBase(comOldtable, 1, combDataBaseOld.SelectedValue.ToString());
+
+        }
+
+        private void btn_biaoNew_Click(object sender, EventArgs e)
+        {
+            BindDataBase(comNewtable, 1, combDataBaseNew.SelectedValue.ToString());
+        }
+        //绑定grid控件
+        public void GetTableZiDuan()
+        {
+            string strSql = string.Format("use {0}  Select Name FROM SysColumns Where id=Object_Id('{1}')", combDataBaseOld.SelectedValue, comOldtable.SelectedValue);
+            this.dataGridView1.DataSource = GetDataBases(strSql).Tables[0];
+
+        }
+        //绑定combobox控件
+        public void GetNewTableZiDuan()
+        {
+            string strSql = string.Format("use {0}  Select Name FROM SysColumns Where id=Object_Id('{1}')", combDataBaseNew.SelectedValue, comNewtable.SelectedValue);
+            this.NewZiDuan.DataSource = GetDataBases(strSql).Tables[0];
+            this.NewZiDuan.ValueMember = "name";
+            this.NewZiDuan.DisplayMember = "name";
+        }
+        private void btn_OldZiDuan_Click(object sender, EventArgs e)
+        {
+            GetTableZiDuan();
+        }
+
+        private void btn_NewZiDuan_Click(object sender, EventArgs e)
+        {
+            GetNewTableZiDuan();
+        }
+        private void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            if (this.dataGridView1.Rows.Count != 0)
+            {
+                for (int i = 0; i < this.dataGridView1.Rows.Count; )
+                {
+                    this.dataGridView1.Rows[i].DefaultCellStyle.BackColor = System.Drawing.Color.Pink;
+                    i += 2;
+                }
+            }
+        }
+
+        private void btn_StartZhuanLu_Click(object sender, EventArgs e)
+        {
+            List<string> oldList = new List<string>();
+            List<string> newList = new List<string>();
+            for (int i = 0; i < this.dataGridView1.Rows.Count; i++)
+            {
+                if ((bool)(((DataGridViewCheckBoxCell)this.dataGridView1.Rows[i].Cells["isTrue"]).EditedFormattedValue) == true)
+                {
+                    oldList.Add(dataGridView1.Rows[i].Cells["OldZiDuan"].Value.ToString());
+                    newList.Add(((DataGridViewComboBoxCell)dataGridView1.Rows[i].Cells["NewZiDuan"]).Value.ToString());
+                }
+            }
+           OldToNew(oldList,newList,"");
+        }
+        private void OldToNew(List<string> oldList, List<string> newList, string where)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("use {0}",combDataBaseOld.SelectedValue);
+            sb.AppendFormat(" select ");
+            for (int i = 0; i < oldList.Count; i++)
+            {
+                sb.AppendFormat(oldList[i] + ",");
+            }
+            sb.Remove(sb.ToString().LastIndexOf(','), 1);
+            sb.AppendFormat(" from {0}", comOldtable.SelectedValue);
+            if (!string.IsNullOrEmpty(where))
+            {
+                sb.AppendFormat(" where {0}", where);
+            }
+            MessageBox.Show(sb.ToString());
+           DataTable dt= GetDataBases(sb.ToString()).Tables[0];
+           if (dt.Rows.Count > 0)
+           {
+               int sum = 0;
+               for (int i = 0; i < dt.Rows.Count; i++)
+               {
+                 sum+=InsertNewDataBase(dt,i,newList,oldList);
+               }
+               if (sum == dt.Rows.Count)
+               {
+                   MessageBox.Show("数据转录成功");
+               }
+               else
+               {
+                   if (sum != 0)
+                   {
+                       MessageBox.Show("理论转录信息条数：" + dt.Rows.Count + "；实践转录信息条数：" + sum + ";实际转录条数与理论条数不符");
+                   }
+                   
+               }
+           }
+           else 
+           {
+               MessageBox.Show("要转录的旧数据库，没有数据信息");
+           }
+        }
+        private int  InsertNewDataBase(DataTable dt,int a,List<string> newList,List<string> oldList)
+        {
+            int sum = 0;
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendFormat("use {0}", combDataBaseNew.SelectedValue);
+                sb.AppendFormat(" insert into {0} (", comNewtable.SelectedValue);
+                for (int i = 0; i < newList.Count; i++)
+                {
+                    sb.AppendFormat(newList[i] + ",");
+                }
+                sb.Remove(sb.ToString().LastIndexOf(','), 1);
+                sb.AppendFormat(")values(");
+                for (int i = 0; i < oldList.Count; i++)
+                {
+                    sb.AppendFormat("'" + dt.Rows[a][oldList[i]] + "'" + ",");
+                }
+                sb.Remove(sb.ToString().LastIndexOf(','), 1);
+                sb.AppendFormat(")");
+                MessageBox.Show(sb.ToString());
+                sum = GetDoIt(sb.ToString());
+                return sum;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return sum;
+            }
+           
+        }
+    }
+}
